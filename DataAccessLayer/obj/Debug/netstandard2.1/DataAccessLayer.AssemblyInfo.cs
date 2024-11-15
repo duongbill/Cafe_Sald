cf70b99a69d0cd3d3868d3f3fb74e6d6bbafb59c
duongbill <billduongg@gmail.com>
2024-11-16 02:48:11 +0700

    Remove .vs folder and update other changes

--- DataAccessLayer/obj/Debug/netstandard2.1/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/netstandard2.1/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1c69c750ce90bc2b45c9db45e3c4a6e0b55b2ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c13c3ba6ff693221ba73d61ca70f67293a4c648")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
